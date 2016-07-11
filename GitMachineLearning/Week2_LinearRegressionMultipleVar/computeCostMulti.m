a0e5d9e62546572c8b6bb34a94d7cabd9863bb7b
Stephen  Wunsch <wunschsa@mymail.vcu.edu>
2016-07-11 17:27:07 -0400

    Week 2 ML Linear regression w/ Multiple Variables

---
+++ GitMachineLearning/Week2_LinearRegressionMultipleVar/computeCostMulti.m
@@ -0,0 +1,33 @@
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
+%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+%[row, col] = size(X);
+%sumpre = zeros(row, col);
+%x = X(:,2);
+%x2 = X(:,3);
+predictions = X * theta;
+%predictions = theta(1) + (theta(2)*x) + (theta(3)*x2);
+
+%for i = 1:col,
+%    predictions = X(:,i) * theta;
+%    sumpre = sumpre + predictions;
+    
+sqrErrors = (predictions .- y).^2;
+
+J = 1/(2*m) * sum(sqrErrors);
+
+% =========================================================================
+
+end
